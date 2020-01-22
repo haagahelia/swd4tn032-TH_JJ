ece7a1af9835229f4979d28cd73eab685af93804
Teemu H <h01581@haaga-helia.fi>
2020-01-22 17:19:47 +0200

    Kommentti lisätty junalipputehtävään

--- src/viikko1/ehtorakenteet/th/JunalippujenHinta.java
+++ src/viikko1/ehtorakenteet/th/JunalippujenHinta.java
@@ -2,28 +2,47 @@ package viikko1.ehtorakenteet.th;
 
 import java.util.Scanner;
 
+/*
+ * Tässä esimerkissä demonstroidaan sisäkkäistä vaihtoehtorakennetta. Käyttäjän
+ * ensimmäisestä vastauksesta riippuen häneltä joko kysytään toinen kysymys tai
+ * tulostetaan vastaus suoraan. Toisen kysymyksen vastauksesta riippuen
+ * suoritetaan jompi kumpi vaihtoehtoisista lohkoista.
+ * 
+ * DISCLAIMER: esimerkki on kirjoitettu suurempia suunnittelematta, ja
+ * esimerkiksi kuukausilipun ja kertalippujen hintojen tulisi olla määritelty
+ * muuttujissa, eikä kirjoitettu useisiin eri kohtiin ohjelmakoodissa.
+ */
 public class JunalippujenHinta {
 
-    // 1. Kysytään ostaako käyttäjä kuukausilipun vai kertalippuja
-    // 2. Jos ostaa kuukausilipun, kerrotaan kuukausihinta
-    // 3. Jos kertalippuja, kysytään kuinka monta, ja kerrotaan yhteishinta
+    /*
+     * 1. Kysytään ostaako käyttäjä kuukausilipun vai kertalippuja
+     * 
+     * 2. Jos ostaa kuukausilipun, kerrotaan kuukausihinta
+     * 
+     * 3. Jos kertalippuja, kysytään kuinka monta, ja kerrotaan tulisiko
+     * kuukausilippu halvemmaksi
+     */
     public static void main(String[] args) {
+        final int kertalipunHinta = 10;
+        final int kuukausilipunHinta = 100;
+
         Scanner lukija = new Scanner(System.in);
 
         System.out.println("Ostatko kuukausilipun (1) vai kertalippuja (2)?");
         int vastaus = lukija.nextInt();
 
         if (vastaus == 1) {
-            System.out.println("Kuukausilippu maksaa 100 €");
+            System.out.println("Kuukausilippu maksaa " + kuukausilipunHinta + " €");
         } else {
             System.out.println("Kuinka monta kertalippua käytät kuukaudessa?");
-            int kertaLippujenMaara = lukija.nextInt();
-            int hinta = kertaLippujenMaara * 10;
+            int kertalippuja = lukija.nextInt();
+            int kokonaishinta = kertalippuja * kertalipunHinta;
 
-            if (hinta > 100) {
+            if (kokonaishinta > kuukausilipunHinta) {
                 System.out.println("Kuukausilippu olisi halvempi");
             } else {
-                System.out.println("Säästät " + (100 - hinta) + " € verrattuna kuukausilippuun");
+                int saasto = kuukausilipunHinta - kokonaishinta;
+                System.out.println("Säästät " + saasto + " € verrattuna kuukausilippuun");
             }
         }
 
