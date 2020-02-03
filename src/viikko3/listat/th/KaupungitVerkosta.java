3cea78d992f569161ba8526d84b32a994309b0db
Teemu H <h01581@haaga-helia.fi>
2020-02-03 14:06:52 +0200

    Kaupungit-tehtävän kommentit

--- src/viikko3/listat/th/KaupungitVerkosta.java
+++ src/viikko3/listat/th/KaupungitVerkosta.java
@@ -3,22 +3,39 @@ package viikko3.listat.th;
 import java.net.URL;
 import java.util.Scanner;
 
+/**
+ * Tällä luokalla luetaan verkosta CSV-tiedostoa, joka sisältää kaupunkien nimet
+ * ja väkiluvun pilkulla eroteltuna: https://git.io/Jv3N3
+ * 
+ * Tietolähde: Luettelo Suomen kaupungeista, Wikipedia.
+ * https://fi.wikipedia.org/wiki/Luettelo_Suomen_kaupungeista. Lisenssi: CC
+ * BY–SA 3.0
+ */
 public class KaupungitVerkosta {
 
+    /*
+     * URL-luokka heittää ns. poikkeuksia virhetilanteissa, kuten verkkoyhteyden
+     * epäonnistuessa. Tämän vuoksi joudumme määrittelemään main-metodiin lisäksi
+     * "throws Exception".
+     */
     public static void main(String[] args) throws Exception {
         URL csvTiedosto = new URL("https://git.io/Jv3N3");
-        Scanner lukija = new Scanner(csvTiedosto.openStream());
 
+        // Luetaan Scannerin avulla netistä ladattua tietovirtaa System.in sijasta:
+        Scanner lukija = new Scanner(csvTiedosto.openStream(), "utf-8");
+
+        // Tiedoston ensimmäinen rivi sisältää vain otsikkotiedot. Niitä ei tarvita.
         String ekaRivi = lukija.nextLine();
-        System.out.println(ekaRivi);
 
+        // Käydään läpi rivejä, kunnes Scanner kertoo rivien loppuneen:
         while (lukija.hasNextLine()) {
             String rivi = lukija.nextLine();
 
+            // Katkaistaan rivi pilkun kohdalta:
             int loppuindeksi = rivi.indexOf(",");
             String kaupunki = rivi.substring(0, loppuindeksi);
-
             String vakilukuString = rivi.substring(loppuindeksi + 1);
+
             int vakiluku = Integer.parseInt(vakilukuString);
 
             if (vakiluku >= 10_000) {
