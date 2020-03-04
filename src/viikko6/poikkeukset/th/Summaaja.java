0b797cf3d7ffdbdebc7b8f89623128020c4b8feb
TH <h01581@haaga-helia.fi>
2020-03-04 10:41:29 +0200

    Summaaja-tehtävän pseudokoodi

---
+++ src/viikko6/poikkeukset/th/Summaaja.java
@@ -0,0 +1,30 @@
+package viikko6.poikkeukset.th;
+
+public class Summaaja {
+
+    public static void main(String[] args) {
+
+        int summa = 0;
+        // Toista koodia toistaiseksi
+        while (true) {
+
+            // kysy syötettä (string-muodossa eli nextLine:llä)
+            //
+            // try -rakenne:
+            //
+            // - muuta syöte kokonaisluvuksi (Integer.parseInt)
+            //
+            // catch -rakenne:
+            //
+            // - tulosta virheteksti jos syöte ei ollut kokonaisluku
+            //
+            // tarkista oliko luku nolla
+            //
+            // - jos oli nolla, tee break)
+            //
+            // - jos ei ollut nolla, kasvata summaa
+        }
+
+        // tulosta summa
+    }
+}
