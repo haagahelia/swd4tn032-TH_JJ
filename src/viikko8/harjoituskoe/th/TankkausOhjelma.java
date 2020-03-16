c871d8b64296d672efd52bfbfeb440e59a658583
TH <h01581@haaga-helia.fi>
2020-03-16 08:25:02 +0200

    Tankkaus

---
+++ src/viikko8/harjoituskoe/th/TankkausOhjelma.java
@@ -0,0 +1,12 @@
+package viikko8.harjoituskoe.th;
+
+public class TankkausOhjelma {
+
+    public static void main(String[] args) {
+        Tankkaus t = new Tankkaus();
+    }
+}
+
+class Tankkaus {
+
+}
